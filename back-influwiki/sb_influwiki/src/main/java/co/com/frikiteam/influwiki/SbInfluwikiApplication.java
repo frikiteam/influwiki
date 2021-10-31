014f6f572cf4327acd113631e27a0b2251ccd8bb
Yesid Rangel <yety93@hotmail.com>
2021-10-31 15:32:51 -0500

    Dev: Creación del proyecto base del backen de influwiki con Spring tool

---
+++ back-influwiki/sb_influwiki/src/main/java/co/com/frikiteam/influwiki/SbInfluwikiApplication.java
@@ -0,0 +1,13 @@
+package co.com.frikiteam.influwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbInfluwikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbInfluwikiApplication.class, args);
+	}
+
+}
---
+++ back-influwiki/sb_influwiki/src/test/java/co/com/frikiteam/influwiki/SbInfluwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.frikiteam.influwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbInfluwikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
