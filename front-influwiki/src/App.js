fdbe6891615aec0a5ed240a6cf7c2e34b356c70a
Carlos Gomez <carlosandres0741@gmail.com>
2021-12-07 08:31:33 -0500

    Add: Register route

--- front-influwiki/src/App.js
+++ front-influwiki/src/App.js
@@ -1,21 +1,22 @@
-import './index.css';
-import './App.css';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {useEffect, useState} from "react";
+
 import Home from "./pages/Home";
 import Explorer from "./pages/Explorer";
 import Profile from "./pages/Profile";
-import {useEffect, useState} from "react";
 import Register from "./pages/Register";
 import Login from './pages/Login';
 
+import './index.css';
+import './App.css';
+import UsuarioService from "./services/UsuarioService";
+
 const App = () => {
 
-  let [data, setData] = useState([])
+  let [users, setUsers] = useState([])
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users/')
-      .then(response => response.json())
-      .then(json => setData(json))
+    UsuarioService.getUsers(setUsers)
   }, [])
 
   return (
@@ -23,8 +24,8 @@ const App = () => {
       <Router>
         <Routes>
           <Route path={'/'} element={<Home/>}/>
-          <Route path={'/explorer'} element={<Explorer data={data}/>}/>
-          <Route path={'/profile/:id'} element={<Profile data={data}/>}/>
+          <Route path={'/explorer'} element={<Explorer users={users}/>}/>
+          <Route path={'/profile/:id'} element={<Profile/>}/>
           <Route path={'/register'} element ={<Register />}/>
           <Route path={'/login'} element ={<Login />}/>
         </Routes>
